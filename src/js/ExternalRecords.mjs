70d2b4d8507a33c802721dba2e60480824b96a09
Ryan Eakin <RyanEakin@protonmail.com>
2026-04-15 01:12:11 -0600

    another-attempt-at-getting-the-api-key-to-not-leak-commit

--- src/js/ExternalRecords.mjs
+++ src/js/ExternalRecords.mjs
@@ -3,8 +3,7 @@ import { setLocalStorage } from "./utils.mjs";
 const movieURL = "https://api.themoviedb.org/3/";
 const showURL = "https://api.tvmaze.com/shows";
 
-const keyOne = process.env.TMDBKEY;
-
+const keyOne = import.meta.env.TMDBKEY;
 
 async function convertToJson(res) {
     if (!res.ok) {
