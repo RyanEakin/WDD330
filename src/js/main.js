08aa31c348c16929886df4f60dda7beaf5abc2af
Ryan Eakin <RyanEakin@protonmail.com>
2026-04-15 19:00:39 -0600

    changed-main-javasript-commit

--- src/js/main.js
+++ src/js/main.js
@@ -10,17 +10,17 @@ LoadHeaderFooter();
 marathon.getMediaData("movie", "", "");
 marathon.getMediaData("movie", "popular", "");
 marathon.getMediaData("movie", "now_playing", "");
+marathon.getMediaData("", "?page=", "1");
 
 const MovieRequest = getLocalStorage("tmdb_request");
 const MoviePopularRequest = getLocalStorage("tmdb_pop_request");
 const MoviePlayRequest = getLocalStorage("tmdb_play_request");
-const tvList = getLocalStorage("tvmaze_request");
 
 const mediaL = new Recommendations(ids("movie")); // made it simplier, because... renderList is the ONLY function that needs datasource
-mediaL.renderList(MovieRequest, tvList);
+mediaL.renderList(MovieRequest);
 
 const mediaL_pop = new Recommendations(ids("popular")); // made it simplier, because... renderList is the ONLY function that needs datasource
-mediaL_pop.renderList(MoviePopularRequest, tvList);
+mediaL_pop.renderList(MoviePopularRequest);
 
 const mediaL_play = new Recommendations(ids("now_playing")); // made it simplier, because... renderList is the ONLY function that needs datasource
-mediaL_play.renderList(MoviePlayRequest, tvList);
+mediaL_play.renderList(MoviePlayRequest);
